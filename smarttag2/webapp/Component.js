d312ae5ee0e361e37a8cf97dc584c5406559ab26
RagavendranMRN <ragavendran1371997@gmail.com>
2020-04-23 11:12:18 +0000

    Smart Tag UI

---
+++ smarttag2/webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("smarttag2.smarttag2.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
